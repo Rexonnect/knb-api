8bbcdad8e794c3cc60f68e451883f65ecf738d41
Rexonnect <rexonnect@protonmail.com>
2023-09-06 17:47:04 +1000

    Update md5hash.js

---
+++ middleware/md5hash.js
@@ -0,0 +1,16 @@
+const crypto = require('crypto');
+
+function md5Middleware(req, res, next) {
+  const { value } = req.body;
+
+  if (!value) {
+    return res.status(400).send("Error: 'value' parameter is missing in the request");
+  }
+
+  // Calculate MD5 hash
+  const md5Hash = crypto.createHash('md5').update(value).digest('hex');
+
+  res.send(md5Hash);
+}
+
+module.exports = md5Middleware;
