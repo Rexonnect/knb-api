c8427dffe59bd6a11c801796c879048ac3d7299f
Rexonnect <rexonnect@protonmail.com>
2023-09-07 17:04:58 +1000

    Update index.js

--- index.js
+++ index.js
@@ -18,8 +18,15 @@ app.post('/hash', md5hash);
 
 app.post('/message', async (req, res) => {
   const { message } = req.body;
+  console.log('Sending message:', JSON.stringify(message));
+
   const webhookUrl = "https://discord.com/api/webhooks/1148881222261547018/eLk0DyWLT9b0GpWUQIosRwPFEfg15LZr5py5BYICP5WpNyTgJRKZXpuFd5EOXCpdmD8H";
   sendWebhookMessage(message, webhookUrl); // Use the correct function name
+  console.log(message + webhookUrl);
+  if (error){
+    console.error('Error sending message to the webhook:', response.status);
+    console.error('Response body:', await response.text());
+  };
 });
 
 app.listen(process.env.PORT || 3000)
